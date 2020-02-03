72d4bbb9b216244ea00a19c35dcb64e5deaf4ddc
Sp00nyMan <48148134+Sp00nyMan@users.noreply.github.com>
2020-02-03 07:55:01 +0300

    Nothing good

--- src/Backpropogation.java
+++ src/Backpropogation.java
@@ -5,7 +5,7 @@ public class Backpropogation
 {
 	private static final double learningRate = 1.05;
 	private static int maxEpochs = 100000;
-	private static double minAccuracy = 0.00001;
+	private static double minAccuracy = 0.001;
 
 	public static void setMaxEpochs(int newEpochs)
 	{
